7806e441c0918b0f252a5bbd3225ffced13ec55f
Nick Downs <nickryand@gmail.com>
2016-02-21 07:26:16 +0000

    Major version bump and breaking changes.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nickryand@gmail.com'
 license 'Apache 2.0'
 description 'Defines a number of LWRP wrapper commands around the awscli command line script'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.2'
+version '2.0.0'
 source_url 'https://github.com/nickryand/awscli-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/nickryand/awscli-cookbook/issues' if respond_to?(:issues_url)
 

ba90f141602567b79b8e5c50ee8b356a037f442d
Nick Downs <nickryand@gmail.com>
2016-02-23 04:52:24 +0000

    Continued work on integration tests

--- test/fixtures/cookbooks/test_credentials/recipes/default.rb
+++ test/fixtures/cookbooks/test_credentials/recipes/default.rb
@@ -11,8 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 #
-include_recipe 'awscli::default'
-
 user 'testuser'
 
 directory '/etc/aws' do
@@ -31,10 +29,40 @@ awscli_credentials '/etc/aws/credentials' do
 end
 
 
+# The second call overwrites the user/group/mode from the first resource
+# call
 awscli_credentials '/etc/aws/credentials' do
+  user 'root'
+  group 'root'
+  mode 0660
   profile 'secondary'
   params(
     region: 'eu-west-2',
     role_arn: 'arn:aws:iam::123456789012:role/testingchef'
+  )
+end
+
+directory '/home/testuser/.aws/' do
+  user 'testuser'
+  group 'testuser'
+  recursive true
+end
+
+s3_config = <<EOF
+
+  max_concurrent_requests=20
+  max_queue_size=10000
+  multipart_threshold=64MB
+  multipart_chunksize=16MB
+EOF
+
+awscli_credentials '/home/testuser/.aws/credentials' do
+  user 'testuser'
+  group 'testuser'
+  mode 0600
+  params(
+    aws_access_key_id: 'TEST123',
+    aws_secret_access_key: 'SECRETKEY!',
+    s3: s3_config
   )
 end

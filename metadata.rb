0f51cd87930e246ab3952fc325ed7704eb0a04f0
Christopher Licata <cmlpolyglotdev@gmail.com>
2017-03-07 09:20:25 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name 'cloudcli'
-maintainer 'Nick Downs'
-maintainer_email 'nickryand@gmail.com'
+maintainer 'Christopher Licata'
+maintainer_email 'cmlpolyglotdev@gmail.com'
 license 'Apache 2.0'
 description 'Install and configure cloud provider CLI tools.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
-source_url 'https://github.com/nickryand/cloudcli-cookbook' if respond_to?(:source_url)
-issues_url 'https://github.com/nickryand/cloudcli-cookbook/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/cmlicata/cloudcli-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/cmlicata/cloudcli-cookbook/issues' if respond_to?(:issues_url)
 
 supports 'ubuntu'
 supports 'centos'

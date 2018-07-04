f3c1ebeab5ec3b8110883788c765a1d2b5e1cd5a
ryym <ryym.64@gmail.com>
2018-07-04 12:00:43 +0900

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cmlpolyglotdev@gmail.com'
 license 'Apache 2.0'
 description 'Install and configure cloud provider CLI tools.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.0'
+version '1.3.0'
 source_url 'https://github.com/cmlicata/cloudcli-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/cmlicata/cloudcli-cookbook/issues' if respond_to?(:issues_url)
 
